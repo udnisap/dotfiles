109bbd266e6730a83b0d40f28caf4d1e87c073c7
Pasindu Perera <perera.pasindu@gmail.com>
2016-11-09 11:57:57 +0530

    setup script updated to support curl execution

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dotfiles
+
+## Install
+```
+bash <(curl -s https://raw.githubusercontent.com/udnisap/dotfiles/master/setup.sh)
+```
--- setup.sh
+++ setup.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
+# Install 
+# apt-get install git ack-grep -y
 
 PWD=`pwd`
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+
 # setup symlinks
-ln -s $PWD/vim/.vimrc ~/.vimrc
-ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf 
-ln -s $PWD/git/.gitconfig ~/.gitconfig 
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [Y/n]}? " response
+    response=${response:-'Y'}
+    case $response in
+        [yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+function installVimBinaryPackages {
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  
+}
+confirm "Symlink vim" && echo 'test'
+confirm "Install Vim Binary Addons" && installVimBinaryPackages
+confirm "Symlink vim" && ln -s $PWD/vim/.vimrc ~/.vimrc
+confirm "Symlink tmux" && ln -s $PWD/tmux/.tmux.conf ~/.tmux.conf 
+confirm "Symlink git" && ln -s $PWD/git/.gitconfig ~/.gitconfig 
+
+
